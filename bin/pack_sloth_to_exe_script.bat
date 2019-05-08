bfe0f5008c029e9794309a877086d7708932e06f
Nightmare4214 <37721929+Nightmare4214@users.noreply.github.com>
2019-05-08 14:26:24 +0800

    Update pack_sloth_to_exe_script.bat

--- bin/pack_sloth_to_exe_script.bat
+++ bin/pack_sloth_to_exe_script.bat
@@ -1,29 +1,21 @@
-set sloth="E:\sloth"
 set anaconda_sloth="D:\Anaconda3\Lib\site-packages\sloth"
-set destination="E:\sloth_test"
-
-if exist %sloth%\bin\build (
-  rmdir /S /Q  %sloth%\bin\build
+::set destination="D:\sloth_test"
+if exist %anaconda_sloth%\bin\build (
+  rmdir /S /Q %anaconda_sloth%\bin\build
 )
-if exist %sloth%\bin\dist (
-  rmdir /S /Q %sloth%\bin\dist
+if exist %anaconda_sloth%\bin\dist (
+  rmdir /S /Q %anaconda_sloth%\bin\dist
 )
-
-xcopy %sloth%\. %anaconda_sloth% /S /E /Y
-rmdir /S /Q  %anaconda_sloth%\.idea
-rmdir /S /Q  %anaconda_sloth%\__pycache__
-rmdir /S /Q  %anaconda_sloth%\bin
-rmdir /S /Q  %anaconda_sloth%\gui\__pycache__
-rmdir /S /Q  %anaconda_sloth%\gui\icons
-del %anaconda_sloth%\gui\icons.qrc
-del %anaconda_sloth%\gui\labeltool.ui
-cd %sloth%\bin
+set destination="D:\sloth_test"
 pyinstaller sloth.spec
 rmdir /S /Q build
+if not exist %destination% (
+  mkdir %destination%
+)
 if not exist %destination%\sloth (
   mkdir %destination%\sloth
 )
-xcopy dist\sloth\. %destination%\sloth /S /E /Y
+xcopy /S /E /Y dist\sloth\. %destination%\sloth
 rmdir /S /Q dist
-cd %destination%
-7z a sloth.7z sloth
+7z a %destination%\sloth.7z %destination%\sloth
+explorer %destination%
